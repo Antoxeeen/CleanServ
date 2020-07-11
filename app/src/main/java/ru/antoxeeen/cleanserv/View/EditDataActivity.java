109fc60b904bd2453ada460de98c65c9c799d9ba
Anton Bykov <nesh90210@gmail.com>
2020-07-11 23:56:37 +0600

    Создал новую активити без дизайна и настроек. Создал новый layout для разметки recyclerView.

---
+++ app/src/main/java/ru/antoxeeen/cleanserv/View/EditDataActivity.java
@@ -0,0 +1,26 @@
+package ru.antoxeeen.cleanserv.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ru.antoxeeen.cleanserv.R;
+
+import android.os.Bundle;
+
+public class EditDataActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_data);
+
+        initVariable();
+
+
+    }
+
+    private void initVariable(){
+
+    }
+
+}
