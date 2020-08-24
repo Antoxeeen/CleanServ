6a1fa911e73ec3232928300307a0739e827be067
Anton Bykov <nesh90210@gmail.com>
2020-08-24 14:03:05 +0600

    Наведение порядк в файле Gradle

---
+++ app/src/main/java/ru/antoxeeen/cleanserv/View/StartActivity.java
@@ -0,0 +1,15 @@
+package ru.antoxeeen.cleanserv.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ru.antoxeeen.cleanserv.R;
+
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
